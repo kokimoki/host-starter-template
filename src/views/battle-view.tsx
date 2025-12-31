b51bea3e30dc841263677ade5b4b273f5f060598
Indrek Leede <leede.indrek@gmail.com>
2025-12-31 15:44:20 +0200

    Battle view layout

--- src/views/battle-view.tsx
+++ src/views/battle-view.tsx
@@ -1,5 +1,6 @@
 import { globalStore } from '@/state/stores/global-store';
 import { useSnapshot } from '@kokimoki/app';
+import { HeartIcon, ShieldIcon } from 'lucide-react';
 import React from 'react';
 
 interface Props {
@@ -13,36 +14,38 @@ export const BattleView: React.FC<React.PropsWithChildren<Props>> = ({
 
 	return (
 		<>
-			<div className="grid w-full flex-none grid-cols-2 justify-items-center">
+			<div className="grid w-full grid-cols-2 justify-items-center gap-12">
 				{teams.map((team, index) => {
 					const barColor = index === 0 ? 'bg-red-500' : 'bg-blue-500';
 					const healthColor = index === 0 ? 'text-red-600' : 'text-blue-600';
 
 					return (
-						<div key={index}>
-							<span className="text-lg font-bold text-slate-900">
-								{team.name}
-							</span>
-							<div className="flex items-center gap-4">
-								<div className="flex items-center gap-2">
-									<span className="text-xs text-slate-600">HP:</span>
-									<span className={`text-sm font-bold ${healthColor}`}>
+						<div key={index} className="w-full">
+							<article className="prose lg:prose-lg xl:prose-xl 2xl:prose-2xl">
+								<h1>{team.name}</h1>
+
+								<h2>
+									<span
+										className={`inline-flex items-center gap-2 ${healthColor}`}
+									>
+										<HeartIcon className="size-8 lg:size-12 xl:size-16" />
 										{team.health}
 									</span>
-								</div>
-								<div className="flex items-center gap-2">
-									<span className="text-xs text-slate-600">Armor:</span>
-									<span className="text-sm font-bold text-slate-900">
+									<span
+										className={`ml-4 inline-flex items-center gap-2 lg:ml-8 xl:ml-12`}
+									>
+										<ShieldIcon className="size-8 lg:size-12 xl:size-16" />
 										{team.armor}
 									</span>
+								</h2>
+
+								<div className="h-12 w-full overflow-hidden rounded-full bg-slate-50 shadow-xs lg:h-20">
+									<div
+										className={`h-full transition-all duration-300 ${barColor}`}
+										style={{ width: `${team.health}%` }}
+									/>
 								</div>
-							</div>
-							<div className="h-3 w-48 overflow-hidden rounded-full bg-slate-200">
-								<div
-									className={`h-full transition-all duration-300 ${barColor}`}
-									style={{ width: `${team.health}%` }}
-								/>
-							</div>
+							</article>
 						</div>
 					);
 				})}
